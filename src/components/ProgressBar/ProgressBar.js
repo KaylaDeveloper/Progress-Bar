9083ed515a52d6aa886adaf8923a0ec03b48f205
Kayla Lee <kaylalee2326@gmail.com>
2022-08-23 19:37:05 +1000

    Progress Bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,48 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZE = {
+  small: {
+    "--height": "8px",
+    "--border-radius": "4px",
+  },
+  medium: {
+    "--height": "12px",
+    "--border-radius": "4px",
+  },
+  large: {
+    "--height": "16px",
+    "--border-radius": "8px",
+    "--padding": "4px",
+  },
 };
 
+const ProgressBar = ({ value, size }) => {
+  const styles = SIZE[size];
+  return (
+    <Wrapper style={styles}>
+      <Progress value={value}></Progress>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 370px;
+  height: var(--height);
+  border-radius: var(--border-radius);
+  padding: var(--padding);
+  background: rgba(128, 128, 128, 0.15);
+  box-shadow: inset 0px 2px 4px rgba(128, 128, 128, 0.35);
+`;
+
+const Progress = styled.div`
+  height: 100%;
+  width: ${(props) => props.value}%;
+  background: #4747eb;
+  border-radius: 4px 0px 0px 4px;
+`;
 export default ProgressBar;
