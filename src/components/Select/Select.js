73e372629c7447bac218341e066d5ddae49730c8
Kayla Lee <kaylalee2326@gmail.com>
2022-08-25 12:14:43 +1000

    IconInput and ProgressBar

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,71 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id={"chevron-down"} strokeWidth={1} size={24}></Icon>
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
 
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  -webkit-appearance: none;
+`;
+const PresentationalBit = styled.div`
+  height: 43px;
+  background: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  padding: 12px 52px 12px 16px;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  color: ${COLORS.gray700};
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  width: 24px;
+  height: 24px;
+  margin: auto;
+  top: 0;
+  right: 10px;
+  bottom: 0;
+  pointer-events: none;
+`;
 export default Select;
